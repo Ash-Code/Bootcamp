b6409245e7c34caae2a555b38e35c75ff3acceac
Ashwin Kumar <renegade403@gmail.com>
2015-08-26 17:42:02 +0530

    initial

---
+++ src/CustomLRU.java
@@ -0,0 +1,110 @@
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class CustomLRU<K, V> {
+	private class Entry {
+		V value;
+		K key;
+		Node node;
+
+		Entry(K key, V value) {
+			this.value = value;
+		}
+
+	}
+
+	private class Node {
+		Node prev;
+		Node next;
+		Entry entry;
+
+		Node(Entry entry) {
+			this.entry = entry;
+		}
+
+	}
+
+	private ConcurrentHashMap<K, Entry> map;
+	private int numItems;
+	private Node head;
+	private Node tail;
+
+	public CustomLRU(int numItems) {
+		map = new ConcurrentHashMap<K, Entry>(10000);
+		this.numItems=numItems;
+	}
+
+	public void put(K key, V value) {
+		Entry e = new Entry(key, value);
+		map.put(e.key, e);
+		offer(e);
+	}
+
+	public V get(K key) {
+		if (map.containsKey(key)) {
+			Entry e = map.get(key);
+			offer(e);
+			purge();
+			return e.value;
+		}
+		return null;
+	}
+
+	private void purge() {
+		while (map.size() > numItems) {
+			if (tail != null) {
+				map.remove(tail.entry.key);
+				tail = tail.prev;
+				tail.next = null;
+			} else {
+				System.out.println("Tail is null");
+				break;
+			}
+		}
+	}
+
+	private void offer(Entry e) {
+		// manipulate the linkedlist
+		// Assign tail, check etc
+		if (head != null) {
+			if (head.entry != e) {
+				Node c = e.node;
+				Node node = new Node(e);
+				AtomicReference<Node> ref = new AtomicReference<Node>(e.node);
+				if (ref.compareAndSet(c, node)) {
+					addToHead(node);
+					if (c != null) {
+						removeNode(c);
+					}
+				}
+			}
+
+		} else {
+			addToHead(new Node(e));
+		}
+
+	}
+
+	private void removeNode(Node c) {
+		if (c.prev != null) {
+			c.prev.next = c.next;
+		}
+		if (c.next != null) {
+			c.next.prev = c.prev;
+		}
+	}
+
+	private synchronized void addToHead(Node n) {
+		if (head == null) {
+			tail = n;
+			head = n;
+			n.next = null;
+			n.prev = null;
+			return;
+		}
+		n.next = head;
+		head.prev = n;
+		head = n;
+	}
+
+}
---
+++ src/TestClass.java
@@ -0,0 +1,36 @@
+
+public class TestClass {
+
+	public static void main(String args[]) {
+		final CustomLRU<Integer, String> cache = new CustomLRU<Integer,String>(100);
+	
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				
+				for (int i = 0; i < 10; i++) {
+					cache.put(i, i + "");
+				}
+
+			}
+
+		});
+		t1.start();
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+			
+				for (int i = 0; i < 10; i++) {
+					cache.put(1000 - i, (1000 - i) + "");
+				}
+
+			}
+
+		});
+		t2.start();
+
+	}
+
+}
